58c949ead9dfdb2b860ca251114f710cbeebc8e9
Alberto J Marin <alberto@ajmar.in>
2020-02-02 02:21:51 -0500

    :rotating_light: Fix linter warnings

--- winpath/__init__.py
+++ winpath/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Retrieves common Windows folders as Unicode strings"""
 
 from .main import (
@@ -24,4 +25,28 @@ from .main import (
     get_recent,
 )
 
-__version__ = "201902.1b0"
+__version__ = "202002.1b0"
+
+__all__ = [
+    get_local_appdata,
+    get_appdata,
+    get_desktop,
+    get_programs,
+    get_admin_tools,
+    get_common_admin_tools,
+    get_common_appdata,
+    get_common_documents,
+    get_my_documents,
+    get_cookies,
+    get_history,
+    get_internet_cache,
+    get_my_pictures,
+    get_personal,
+    get_program_files,
+    get_program_files_common,
+    get_system,
+    get_windows,
+    get_favorites,
+    get_startup,
+    get_recent,
+]
